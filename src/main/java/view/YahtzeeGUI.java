785373afbd7db7c89e4a645a54bfe6930cd94420
Paul Henstridge <paulhenstridge@gmail.com>
2023-10-20 15:03:15 +0100

    basic GUI layout done

--- src/main/java/view/YahtzeeGUI.java
+++ src/main/java/view/YahtzeeGUI.java
@@ -1,3 +1,5 @@
+package view;
+
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
@@ -14,7 +16,7 @@ public class YahtzeeGUI extends JFrame {
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));  // Top-to-bottom layout
 
         // Create and add dice panels
-        JPanel dicePanel = new JPanel(new GridLayout(1, 5));  // 1 row, 5 columns
+        JPanel dicePanel = new JPanel(new GridLayout(1, 5, 30, 30));  // 1 row, 5 columns
         for (int i = 0; i < 5; i++) {
             JPanel singleDicePanel = createDicePanel();
             dicePanel.add(singleDicePanel);
@@ -23,11 +25,15 @@ public class YahtzeeGUI extends JFrame {
 
         // Create and add category panels
         String[] upperCategories = {"Aces", "Twos", "Threes", "Fours", "Fives", "Sixes"};
-        String[] lowerCategories = {"Three of a kind", "Four of a kind", "Full House", "Small Straight", "Large Straight", "Yahtzee", "Chance"};
+        String[] lowerCategories = {"Three of a kind", "Four of a kind", "Full House", "Small Straight", "Large Straight", "Yahtzee", "Chance", "Yahtzee Bonus"};
         JPanel upperCategoryPanel = createCategoryPanel("Upper Section", upperCategories);
         JPanel lowerCategoryPanel = createCategoryPanel("Lower Section", lowerCategories);
         mainPanel.add(upperCategoryPanel);
         mainPanel.add(lowerCategoryPanel);
+
+        // Create and add the bottom panel
+        JPanel bottomPanel = createBottomPanel();
+        mainPanel.add(bottomPanel);
 
         // Add main panel to frame's content pane
         add(mainPanel);
@@ -37,25 +43,97 @@ public class YahtzeeGUI extends JFrame {
         setVisible(true);
     }
 
-    private JPanel createDicePanel() {
+//    private JPanel createDicePanel() {
+//        JPanel panel = new JPanel(new BorderLayout());
+//        JLabel diceLabel = new JLabel("1", SwingConstants.CENTER);  // Initial dice value is 1
+//        diceLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+//        JButton holdButton = new JButton("Hold");
+//        panel.add(diceLabel, BorderLayout.CENTER);
+//        panel.add(holdButton, BorderLayout.SOUTH);
+//        return panel;
+//    }
+private JPanel createDicePanel() {
+    JPanel panel = new JPanel(new BorderLayout());
+
+    // Create a wrapper panel with BoxLayout
+    JPanel wrapper = new JPanel();
+    wrapper.setLayout(new BoxLayout(wrapper, BoxLayout.X_AXIS));
+
+    JLabel diceLabel = new JLabel("1", SwingConstants.CENTER);
+    diceLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
+
+    // Set a preferred size for diceLabel to make it square
+    Dimension squareSize = new Dimension(50, 50);
+    diceLabel.setPreferredSize(squareSize);
+    diceLabel.setMaximumSize(squareSize);
+    diceLabel.setMinimumSize(squareSize);
+
+    wrapper.add(Box.createHorizontalGlue());
+    wrapper.add(diceLabel);
+    wrapper.add(Box.createHorizontalGlue());
+
+    JButton holdButton = new JButton("Hold");
+
+    panel.add(wrapper, BorderLayout.CENTER);  // Add wrapper instead of diceLabel
+    panel.add(holdButton, BorderLayout.SOUTH);
+    return panel;
+}
+
+    private JPanel createCategoryPanel(String title, String[] categories) {
         JPanel panel = new JPanel(new BorderLayout());
-        JLabel diceLabel = new JLabel("1", SwingConstants.CENTER);  // Initial dice value is 1
-        JButton holdButton = new JButton("Hold");
-        panel.add(diceLabel, BorderLayout.CENTER);
-        panel.add(holdButton, BorderLayout.SOUTH);
+        panel.setBorder(BorderFactory.createTitledBorder(title));
+        JPanel categoryGrid = new JPanel(new GridLayout(categories.length, 3));
+
+        for (String category : categories) {
+            JLabel categoryLabel = new JLabel(category);
+            JButton selectButton = new JButton("Select");
+            JLabel scoreLabel = new JLabel("");
+            scoreLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+            categoryGrid.add(categoryLabel);
+            categoryGrid.add(selectButton);
+            categoryGrid.add(scoreLabel);
+        }
+        panel.add(categoryGrid, BorderLayout.CENTER);
+
+        JPanel scorePanel = new JPanel(new BorderLayout());
+        JLabel scoreLabel = new JLabel("0", SwingConstants.CENTER);
+        scoreLabel.setFont(new Font("Arial", Font.BOLD, 32));
+
+        scorePanel.add(new JLabel(title + " Score"), BorderLayout.NORTH);
+        scorePanel.add(scoreLabel, BorderLayout.CENTER);
+
+        JLabel bonusLabel = new JLabel("BONUS", SwingConstants.CENTER);
+        bonusLabel.setFont(new Font("Arial", Font.PLAIN, 18));
+        Color labelColor = new Color(200, 200, 200);
+        bonusLabel.setForeground(labelColor);
+        bonusLabel.setBorder(BorderFactory.createLineBorder(labelColor, 1));
+
+        scorePanel.add(bonusLabel, BorderLayout.SOUTH);
+
+        panel.add(scorePanel, BorderLayout.EAST);
+
         return panel;
     }
 
-    private JPanel createCategoryPanel(String title, String[] categories) {
-        JPanel panel = new JPanel(new GridLayout(categories.length, 2));  // Each category with its score
-        panel.setBorder(BorderFactory.createTitledBorder(title));
-        for (String category : categories) {
-            JLabel categoryLabel = new JLabel(category);
-            JLabel scoreLabel = new JLabel("_");
-            panel.add(categoryLabel);
-            panel.add(scoreLabel);
-        }
-        return panel;
+    private JPanel createBottomPanel() {
+        JPanel bottomPanel = new JPanel(new GridLayout(1, 2));
+
+        JPanel scorePanel = new JPanel(new BorderLayout());
+        JLabel scoreLabel = new JLabel("0", SwingConstants.CENTER);
+        scoreLabel.setFont(new Font("Arial", Font.BOLD, 32));
+        scorePanel.add(new JLabel("TOTAL SCORE"), BorderLayout.NORTH);
+        scorePanel.add(scoreLabel, BorderLayout.CENTER);
+
+        JPanel messagePanel = new JPanel(new BorderLayout());
+        JLabel messageLabel = new JLabel("", SwingConstants.CENTER);
+        messageLabel.setFont(new Font("Arial", Font.PLAIN, 18));
+        messagePanel.add(new JLabel("Game Messages"), BorderLayout.NORTH);
+        messagePanel.add(messageLabel, BorderLayout.CENTER);
+
+        bottomPanel.add(scorePanel);
+        bottomPanel.add(messagePanel);
+
+        return bottomPanel;
     }
 
     public static void main(String[] args) {
